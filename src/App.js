9c61ee2c28bcd8aca65f5bf677ae992cb1311db7
github username <github email address>
2017-10-21 22:43:41 -0400

    test buttons

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import React, { Component } from 'react';
 import { Router, browserHistory, Route, Link } from 'react-router';
-import './App.css';
+import Home from './components/home'
 
-const Page = ({ title }) => (
-    <div className="App">
-      <div className="App-header">
-        <h2>{title}</h2>
-      </div>
-      <p className="App-intro">
-        This is the {title} page.
-      </p>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/settings">Settings</Link>
-      </p>
-    </div>
-);
+export default class App extends Component {
 
-const Home = (props) => (
-  <Page title="Home"/>
-);
-
-const About = (props) => (
-  <Page title="About"/>
-);
-
-const Settings = (props) => (
-  <Page title="Settings"/>
-);
-
-class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
         <Route path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/settings" component={Settings}/>
       </Router>
     );
   }
 }
-
-export default App;
