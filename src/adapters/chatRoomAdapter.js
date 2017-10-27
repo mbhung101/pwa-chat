1b27279abe03918eea420726e8c9df270614ecd1
github username <github email address>
2017-10-27 13:37:47 -0400

    back button and local work

--- src/adapters/chatRoomAdapter.js
+++ src/adapters/chatRoomAdapter.js
@@ -1,7 +1,7 @@
 export default class ChatRoomAdapter  {
 
   static gatherChatRooms(user_id){
-    return fetch("https://pwabackend.herokuapp.com/api/chatrooms", {
+    return fetch("http://localhost:3000/api/chatrooms", {
       method: 'POST',
       headers: this.headers(),
       body: JSON.stringify({
@@ -11,7 +11,7 @@ export default class ChatRoomAdapter  {
   }
 
   static enterRoom(name){
-    return fetch("https://pwabackend.herokuapp.com/api/chatrooms", {
+    return fetch("http://localhost:3000/api/chatrooms", {
       method: 'POST',
       headers: this.headers(),
       body: JSON.stringify({
@@ -21,7 +21,7 @@ export default class ChatRoomAdapter  {
   }
 
   static findUser(chatName){
-    return fetch("https://pwabackend.herokuapp.com/api/chatrooms", {
+    return fetch("http://localhost:3000/api/chatrooms", {
       method: 'POST',
       headers: this.headers(),
       body: JSON.stringify({
@@ -32,7 +32,7 @@ export default class ChatRoomAdapter  {
   }
 
   static newChat(patient,dob,phone,room_name,user_id,message){
-    return fetch("https://pwabackend.herokuapp.com/api/chatrooms", {
+    return fetch("http://localhost:3000/api/chatrooms", {
       method: 'POST',
       headers: this.headers(),
       body: JSON.stringify({
